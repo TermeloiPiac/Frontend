030f2abe82367d98f50cf8987c078e06ac3ea6a9
Adam Dara <adam.dara@hotmail.com>
2023-12-17 13:59:08 +0100

    register design

--- TermeloiPiac/src/app/app.routes.ts
+++ TermeloiPiac/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { BodyComponent } from './body/body.component';
 import { SignInComponent } from './body/sign-in/sign-in.component';
+import { RegisterComponent } from './body/register/register.component';
 
 export const routes: Routes = [
   {path: '', component: BodyComponent},
-  {path: 'belepes', component: SignInComponent}
+  {path: 'belepes', component: SignInComponent},
+  {path: 'regisztracio', component: RegisterComponent}
 ];
